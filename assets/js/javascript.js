15e0cf3eea3bf5a92632d4e98f8de60c1525f508
samirarezai <samirarezai1996@gmail.com>
2019-05-05 17:28:27 +0430

    add owl carousel and fixed avbar for scroll

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -13,12 +13,64 @@ $('.bars').click(function(){
 });
 //=======================scroll navbar===================//
 $(window).scroll(function(){
-    if($(this).scrollTop() > 130){
-        $('.navigation').addClass('style-navigation');
+    var y = $(this).scrollTop();
+    if($(this).scrollTop() > 99 & $(window).width() > 992){
+        $('.navigation').fadeIn().addClass('style-navigation');
         $('.navigation .collapse').addClass('style-navbar');
         } else {
         $('.navigation').removeClass('style-navigation');
         $('.navigation .collapse').removeClass('style-navbar');
         }
 });
+
+//============================owl carousel==========================//
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:3,
+    loop:true,
+    margin:10,
+    nav:true,
+    rtl:true,
+    autoplay:true,
+    slideSpeed : 700,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        750:{
+            items:2
+        },
+        1160:{
+            items:3
+        }
+    }
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+//============================hover owl pics==========================//
+    $(".owl-pics .item").hover(function () {
+       $(this).find(".customer").css("transform","scale(1.04,1.04)");
+    },
+        function () {
+            $(this).find(".customer").css("transform","scale(1,1)");
+    }
+    );
+//============================hover owl pics==========================//
+
+
+      /*  $(document).scroll(function () {
+            var y = $(this).scrollTop();
+            if (y > 99) {
+                $('.navigation').fadeIn().css({"position":"fixed","top":"0"});
+            } else {
+                $('.navigation').css({"position" : "static"});
+            }
+        });*/
+    //============================the end==========================//
 });
